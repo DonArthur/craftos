e8af5c5f8c8b6889e45a7b1ed2d942cf0590b013
DonArthur <geraldy.miero@gmail.com>
2026-07-25 01:58:07 +0700

    chore: added theme and router providers, added default color palette

---
+++ frontend/src/app/router/index.tsx
@@ -0,0 +1,32 @@
+/* eslint-disable react-refresh/only-export-components */
+import { createBrowserRouter } from 'react-router-dom';
+
+// We will eventually import these from your 'features' folders!
+// For now, these are placeholder components.
+const Dashboard = () => <div className="p-8">Dashboard Home</div>;
+const Tasks = () => <div className="p-8">Tasks List</div>;
+const Settings = () => <div className="p-8">Settings</div>;
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    // This element will act as your main layout wrapper (Sidebar, Header, etc.)
+    element: (
+      <div className="min-h-screen w-full bg-background">
+        {/* Your Sidebar/Header components will go here */}
+        <main className="flex-1">
+          {/* We will eventually render an <Outlet /> here so child routes inject inside the layout */}
+          <Dashboard />
+        </main>
+      </div>
+    ),
+  },
+  {
+    path: '/tasks',
+    element: <Tasks />,
+  },
+  {
+    path: '/settings',
+    element: <Settings />,
+  },
+]);
