9b53c74b4f78f7c6755fd855c0888aff4b59e54b
DonArthur <geraldy.miero@gmail.com>
2026-07-24 18:59:27 +0700

    chore: added dependencies and setup tailwindcss

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,14 +15,14 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Get started</h1>
+          <h1 className='text-sm font-extrabold'>Get started</h1>
           <p>
             Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
           </p>
         </div>
         <button
           type="button"
-          className="counter"
+          className='hover:bg-cyan-300'
           onClick={() => setCount((count) => count + 1)}
         >
           Count is {count}
